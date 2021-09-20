741c6eb14f3f0aab86f521c8b0e2a3294de3a71e
richelbilderbeek <richel@richelbilderbeek.nl>
2021-09-20 10:45:30 +0200

    Use a semicolon instead of a plus before ggplot2::ggsave, as the plus now gives a rightful error

---
+++ scripts/goera_bok_installera.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Concatenate all Markdown files first, convert those to one PDF
+
+build_folder=build
+#echo $build_folder
+
+rm -rf build
+mkdir build
+
+if [ ! -d $build_folder ]; then 
+  echo "Error: failed to create build folder"
+  exit 1
+fi
+
+
+cp ../kapitel/foerord/*.* $build_folder             ; mv $build_folder/README.md $build_folder/README_00.md
+cp ../kapitel/installera_processing/*.* $build_folder ; mv $build_folder/README.md $build_folder/README_01.md
+# cp ../kapitel/flytta_bollen_till_hoeger/*.* $build_folder  ; mv $build_folder/README.md $build_folder/README_02.md
+# cp ../kapitel/width_och_height/*.* $build_folder    ; mv $build_folder/README.md $build_folder/README_03.md
+# cp ../kapitel/point_och_random/*.* $build_folder    ; mv $build_folder/README.md $build_folder/README_04.md
+
+cd $build_folder
+
+cat README_00.md >> README.md; echo " " >> README.md; echo "\pagebreak" >> README.md; echo " " >> README.md
+cat README_01.md >> README.md; echo " " >> README.md; echo "\pagebreak" >> README.md; echo " " >> README.md
+
+pandoc README.md -o bok.pdf --toc --toc-depth=1 --highlight-style=tango -V geometry:margin=0.5in
+cp bok.pdf ../../boecker/bok_installera_utan_framsida.pdf
+
+cd ../../boecker
+pdfunite bok_framsida_1.pdf bok_installera_utan_framsida.pdf bok_installera.pdf
+
+# Make booklet
+bookletimposer -a bok_installera.pdf -o haefte_installera.pdf
+
+rm bok_installera_utan_framsida.pdf
