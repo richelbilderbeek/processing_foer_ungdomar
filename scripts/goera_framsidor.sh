a3605ae2620b94fedd178876beb5e03f5ecf14f9
richelbilderbeek <richel@richelbilderbeek.nl>
2021-10-17 13:03:31 +0200

    Third book title page done

--- scripts/goera_framsidor.sh
+++ scripts/goera_framsidor.sh
@@ -15,9 +15,9 @@ fi
 cp ../kapitel/framsidor/*.* $build_folder
 cd $build_folder
 
-pandoc framsida_1.md -o ../../boecker/bok_framsida_1.pdf
-pandoc framsida_2.md -o ../../boecker/bok_framsida_2.pdf
-#pandoc framsida_3.md -o ../../boecker/bok_framsida_3.pdf
+#pandoc framsida_1.md -o ../../boecker/bok_framsida_1.pdf
+#pandoc framsida_2.md -o ../../boecker/bok_framsida_2.pdf
+pandoc framsida_3.md -o ../../boecker/bok_framsida_3.pdf
 #pandoc framsida_4.md -o ../../boecker/bok_framsida_4.pdf
 #pandoc framsida_5.md -o ../../boecker/bok_framsida_5.pdf
 #pandoc framsida_6.md -o ../../boecker/bok_framsida_6.pdf
