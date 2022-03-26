b0918eada8575c4e7706f7b78c44c9a73e7cd70f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-03-26 12:58:25 +0100

    EOD

---
+++ kapitel/skriva/README.md
@@ -0,0 +1,6 @@
+# Skriva
+
+Använder 'Print both sides' och 'Flip on short edges'
+
+![](skriva_annotated.png)
+
--- scripts/goera_test_bok.sh
+++ scripts/goera_test_bok.sh
@@ -15,14 +15,12 @@ fi
 
 
 cp ../kapitel/foerord/*.* $build_folder             ; mv $build_folder/README.md $build_folder/README_00.md
-# cp ../kapitel/ett_vackert_program/*.* $build_folder ; mv $build_folder/README.md $build_folder/README_01.md
-# cp ../kapitel/flytta_bollen_till_hoeger/*.* $build_folder  ; mv $build_folder/README.md $build_folder/README_02.md
-# cp ../kapitel/width_och_height/*.* $build_folder    ; mv $build_folder/README.md $build_folder/README_03.md
-# cp ../kapitel/point_och_random/*.* $build_folder    ; mv $build_folder/README.md $build_folder/README_04.md
+cp ../kapitel/skriva/*.* $build_folder              ; mv $build_folder/README.md $build_folder/README_01.md
 
 cd $build_folder
 
 cat README_00.md >> README.md; echo " " >> README.md; echo "\pagebreak" >> README.md; echo " " >> README.md
+cat README_01.md >> README.md; echo " " >> README.md; echo "\pagebreak" >> README.md; echo " " >> README.md
 
 pandoc README.md -o bok.pdf --toc --toc-depth=1 --highlight-style=tango -V geometry:margin=0.5in
 cp bok.pdf ../../boecker/test_bok_utan_framsida.pdf
