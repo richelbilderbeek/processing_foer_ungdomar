55c4b4ac528098829d28b762bae323cb3339f08f
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-22 06:37:13 +0200

    Front page 9

---
+++ docs/chapters/front_pages/front_page_9.md
@@ -0,0 +1,11 @@
+# Processing
+# Bok 9
+
+![Bok 9](front_page_9.png)
+
+`#`|Beskriving
+---|-----------------------
+33 |Funktioner 1
+34 |Uppräkningar
+35 |Klasser 1
+36 |Funktioner 2
--- docs/scripts/create_front_pages.sh
+++ docs/scripts/create_front_pages.sh
@@ -15,12 +15,13 @@ fi
 cp ../chapters/front_pages/*.* "${build_folder}"
 cd "${build_folder}" || exit 42
 
-pandoc front_page_1.md -o ../../books/book_front_page_1.pdf
-pandoc front_page_2.md -o ../../books/book_front_page_2.pdf
-pandoc front_page_3.md -o ../../books/book_front_page_3.pdf
-pandoc front_page_4.md -o ../../books/book_front_page_4.pdf
-pandoc front_page_5.md -o ../../books/book_front_page_5.pdf
-pandoc front_page_6.md -o ../../books/book_front_page_6.pdf
-pandoc front_page_7.md -o ../../books/book_front_page_7.pdf
-pandoc front_page_8.md -o ../../books/book_front_page_8.pdf
+#pandoc front_page_1.md -o ../../books/book_front_page_1.pdf
+#pandoc front_page_2.md -o ../../books/book_front_page_2.pdf
+#pandoc front_page_3.md -o ../../books/book_front_page_3.pdf
+#pandoc front_page_4.md -o ../../books/book_front_page_4.pdf
+#pandoc front_page_5.md -o ../../books/book_front_page_5.pdf
+#pandoc front_page_6.md -o ../../books/book_front_page_6.pdf
+#pandoc front_page_7.md -o ../../books/book_front_page_7.pdf
+#pandoc front_page_8.md -o ../../books/book_front_page_8.pdf
+pandoc front_page_9.md -o ../../books/book_front_page_9.pdf
 
