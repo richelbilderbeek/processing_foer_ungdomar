9c75cd3304cc2c0d5296d076fe578e301b880181
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-22 06:29:00 +0200

    Create new front pages

--- docs/scripts/create_book_1.sh
+++ docs/scripts/create_book_1.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Concatenate all Markdown files first, convert those to one PDF
+cd ../../../processing_foer_ungdomar/docs/scripts || echo "ERROR: Please run this script from the same folder as where it resides. Tip: 'cd scripts'" || exit 42
+
 
 build_folder=build
-#echo $build_folder
+book_number=1
 
 rm -rf build
 mkdir build
 
-if [ ! -d $build_folder ]; then 
+if [ ! -d ${build_folder} ]; then 
   echo "Error: failed to create build folder"
   exit 1
 fi
 
 
-cp ../chapters/foerord/*.* $build_folder                      ; mv $build_folder/README.md $build_folder/README_00.md
+cp ../chapters/foreword/*.* $build_folder                     ; mv $build_folder/README.md $build_folder/README_00.md
 cp ../chapters/01_ett_vackert_program/*.* $build_folder       ; mv $build_folder/README.md $build_folder/README_01.md
 cp ../chapters/02_flytta_bollen_till_hoeger/*.* $build_folder ; mv $build_folder/README.md $build_folder/README_02.md
 cp ../chapters/03_width_och_height/*.* $build_folder          ; mv $build_folder/README.md $build_folder/README_03.md
@@ -31,12 +32,12 @@ cd "${build_folder}" || exit 42
 } >> README.md
 
 pandoc README.md -o book.pdf --toc --toc-depth=1 --highlight-style=tango -V geometry:margin=0.5in
-cp book.pdf ../../books/book_1_without_front_page.pdf
+cp book.pdf ../../books/book_${book_number}_without_front_page.pdf
 
 cd ../../books || exit 43
-pdfunite book_front_page_1.pdf book_1_without_front_page.pdf book_1.pdf
+pdfunite book_front_page_${book_number}.pdf book_${book_number}_without_front_page.pdf book_${book_number}.pdf
 
 # Make booklet
-bookletimposer -a book_1.pdf -o booklet_1.pdf
+bookletimposer -a book_${book_number}.pdf -o booklet_${book_number}.pdf
 
-rm book_1_without_front_page.pdf
+rm book_${book_number}_without_front_page.pdf
